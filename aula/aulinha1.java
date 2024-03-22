786493a025b976b9fc1c2ae040ff57d41398ad21
rafitaand <rafita.andriotta@gmail.com>
2024-03-22 17:31:47 -0300

    segunda atividade

--- aula/aulinha1.java
+++ aula/aulinha1.java
@@ -1,28 +1,26 @@
- 
+
 package aula;
 
 import java.util.Scanner;
 
 public class aulinha1 {
     
+
     public static void main(String[] args) {
         // Informa ao usuário sobre o propósito do programa
-        System.out.println("Este programa realiza a soma de dois números inteiros.");
+        System.out.println("Este programa realiza a área do quadrado: ");
 
-        // Solicita e recebe o primeiro número do usuário
+       // Informa o pedido do valor do lado desejado, para que descubra a área
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o primeiro número: ");
-        int primeiroNumero = scanner.nextInt();
+        System.out.print("Digite o valor da lateral do quadrado desejado: ");
+        int Lado = scanner.nextInt();
+     
 
-        // Solicita e recebe o segundo número do usuário
-        System.out.print("Digite o segundo número: ");
-        int segundoNumero = scanner.nextInt();
-
-        // Calcula a soma dos dois números
-        int resultadoSoma = primeiroNumero + segundoNumero;
+        // Calcula a área
+        int resultadoArea = (Lado*2);
 
         // Exibe o resultado para o usuário
-        System.out.println("A soma de " + primeiroNumero + " e " + segundoNumero + " é: " + resultadoSoma);
+        System.out.println("o valor da área do quadrado, é igual a: " + resultadoArea);
 
         // Fecha o scanner para evitar vazamento de recursos
         scanner.close();
