011009bc745f06fbe16b0e6f13c22ded3491254e
rafitaand <rafita.andriotta@gmail.com>
2024-03-23 16:34:30 -0300

    perimetroDoRetangulo

--- aula/aulinha1.java
+++ aula/aulinha1.java
@@ -4,25 +4,22 @@ package aula;
 import java.util.Scanner;
 
 public class aulinha1 {
-    
 
     public static void main(String[] args) {
-        // Informa ao usuário sobre o propósito do programa
-        System.out.println("Este programa realiza a área do quadrado: ");
-
-       // Informa o pedido do valor do lado desejado, para que descubra a área
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o valor da lateral do quadrado desejado: ");
-        int Lado = scanner.nextInt();
-     
+    System.out.println("Este programa realiza o perímetro de um retângulo.");
+    
+    System.out.println("Digite o valor da Altura do retângulo: ");
+    int Altura = scanner.nextInt();
+    System.out.println("Digite o valor da base: ");  int base = scanner.nextInt();
+    
+  
 
-        // Calcula a área
-        int resultadoArea = (Lado*2);
-
-        // Exibe o resultado para o usuário
-        System.out.println("o valor da área do quadrado, é igual a: " + resultadoArea);
-
-        // Fecha o scanner para evitar vazamento de recursos
-        scanner.close();
+    int resultadoPerimetro = 2*(Altura+base);
+    System.out.println("O valor encotrado do perímetro do retângulo, foi de: " + resultadoPerimetro);
     }
 }
+
+
+    
+
